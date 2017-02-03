f2d240a6ae7f2c66b31efeb7c4d85d0076c315bc
Stephen Tvedt <stvedt@kbsp.com>
2017-02-03 14:58:58 -0500

    logic for game peices set

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 (function(){
+// console.log('hi');
+
+var Game = function (){
+
+  this.gameOrder = function(){
+    var array = []
+    while (array.length < 16) {
+      var randomnumber = Math.ceil(Math.random()*16)
+      if(array.indexOf(randomnumber) > -1) continue;
+      array[array.length] = randomnumber;
+    }
+
+
+    return array;
+  }
+
+  this.board = this.gameOrder();
+
+  this.new = function () {
+    for (var i = 0; i < this.board.length; i++) {
+      this.peices[i].innerText = this.board[i];
+    }
+  }
+  this.reset = function(){
+
+  }
+  this.move = function(){
+
+  }
+  this.peices = document.querySelectorAll('.peice');
+
+};
+
+var playGame = new Game();
+
+playGame.new();
+
+console.log(playGame.peices);
 
 })();
